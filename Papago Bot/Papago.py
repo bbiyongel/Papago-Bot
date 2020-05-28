ccd0121170f0dc7342ead14123963527bd84974e
Beta5051 <58169688+Beta5051@users.noreply.github.com>
2020-05-28 12:06:49 +0900

    실행 파일

--- Papago Bot/Papago.py
+++ Papago Bot/Papago.py
@@ -1,5 +1,19 @@
 import subprocess
+from tkinter import *
 
-input("실행하려면 엔터를 누르세요.\n")
+window = Tk()
+window.title("Papago")
+window.iconbitmap(default="Papago.ico")
+window.resizable(False, False)
 
-subprocess.call(["python", "source/PapagoMain.py"])
+def Start():
+    window.destroy()
+    subprocess.call(["python", "source/PapagoMain.py"])
+
+label = Label(text="\n본 프로그램은 디스코드 봇 프로그램으로\nPapago API를 사용하였습니다.")
+label.pack()
+
+button = Button(window, text="계속", command=Start)
+button.pack()
+
+window.mainloop()
