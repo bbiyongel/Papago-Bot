b4b6adcadd02333cce01344e63d5580ab4046293
Beta5051 <58169688+Beta5051@users.noreply.github.com>
2020-06-01 14:25:46 +0900

    단순화, 컬러 추가

--- Papago Bot/source/PapagoMain.py
+++ Papago Bot/source/PapagoMain.py
@@ -1,19 +1,22 @@
-import discord
+# Module
+import discord # Disocrd.py
 from discord.ext import commands
 from discord.utils import get
 import asyncio
 import PapagoSub
-from gtts import gTTS
+from gtts import gTTS # Text -> Sound (Google)
 import eyed3
 import os
 
 bot = commands.Bot(command_prefix="/")
 bot.remove_command("help")
 
+# Command
+
+# Start
 @bot.event
 async def on_ready():
-    print("봇이 준비됨!\n")
-    print(f"{PapagoSub.API}\n") 
+    print(f"{PapagoSub.BLUE}{PapagoSub.BOLD}{PapagoSub.API}\n{PapagoSub.END}")
     while True:
         game = discord.Game(PapagoSub.API)
         await bot.change_presence(status=discord.Status.online, activity=game)
@@ -21,21 +24,25 @@ async def on_ready():
         game = discord.Game("/도움말 & /help")
         await bot.change_presence(status=discord.Status.online, activity=game)
         await asyncio.sleep(5)
-    
+
+# Help
 @bot.command(pass_context=True, aliases=["help"])
 async def 도움말(ctx):
     await ctx.author.send(embed=PapagoSub.Tool.Embed(ctx, "도움말", PapagoSub.Tool.TXTRead("helpdescription")))
 
+# Translation
 @bot.command(pass_context=True)
 async def 번역(ctx, lang1, lang2, *, text):
     await ctx.send(embed=PapagoSub.Translation.Embed(ctx, PapagoSub.Tool.LangChange(lang1), PapagoSub.Tool.LangChange(lang2), PapagoSub.Translation.Translation(PapagoSub.Tool.LangChange(lang1), PapagoSub.Tool.LangChange(lang2), text)))
     PapagoSub.Tool.FeedBack(ctx, PapagoSub.Tool.LangChange(lang2), PapagoSub.Tool.LangChange(lang2), text, PapagoSub.Translation.Translation(PapagoSub.Tool.LangChange(lang1), PapagoSub.Tool.LangChange(lang2), text), "Translation")
 
+# Auto Translation
 @bot.command(pass_context=True)
 async def 자동번역(ctx, lang, *, text):
     await ctx.send(embed=PapagoSub.Translation.Embed(ctx, PapagoSub.Translation.Detect(text), PapagoSub.Tool.LangChange(lang), PapagoSub.Translation.Translation(PapagoSub.Translation.Detect(text), PapagoSub.Tool.LangChange(lang), text)))
     PapagoSub.Tool.FeedBack(ctx, PapagoSub.Tool.LangChange(PapagoSub.Translation.Detect(text)), PapagoSub.Tool.LangChange(lang), text, PapagoSub.Translation.Translation(PapagoSub.Translation.Detect(text), PapagoSub.Tool.LangChange(lang), text), "Auto Translation")
 
+# Sound Translation
 @bot.command(pass_context=True)
 async def 음성번역(ctx, lang, *, text):
     out = gTTS(text=PapagoSub.Translation.Translation(PapagoSub.Translation.Detect(text), PapagoSub.Tool.LangChange(lang), text), lang=PapagoSub.Tool.LangChange(lang), slow=False)
@@ -54,12 +61,15 @@ async def 음성번역(ctx, lang, *, text):
     os.remove(f"tts/{ctx.channel.id}.mp3")
     PapagoSub.Tool.FeedBack(ctx, PapagoSub.Tool.LangChange(PapagoSub.Translation.Detect(text)), PapagoSub.Tool.LangChange(lang), text, PapagoSub.Translation.Translation(PapagoSub.Translation.Detect(text), PapagoSub.Tool.LangChange(lang), text), "Auto Translation")
 
+# Language List
 @bot.command(pass_context=True)
 async def 번역언어(ctx):
     await ctx.author.send(embed=PapagoSub.Tool.Embed(ctx, "번역 언어", PapagoSub.Tool.TXTRead("language")))
 
+# API List
 @bot.command(pass_context=True)
 async def API목록(ctx):
     await ctx.author.send(embed=PapagoSub.Tool.Embed(ctx, "API 목록", PapagoSub.Tool.TXTRead("APIdescription")))
-    
+
+# Token
 bot.run(PapagoSub.ConfigRead("option", "token"))
--- Papago Bot/source/PapagoSub.py
+++ Papago Bot/source/PapagoSub.py
@@ -1,41 +1,53 @@
+# Module
 import configparser
-import discord
+import discord # Discord.py
 import requests as rq
 
+# Config
 config = configparser.RawConfigParser()
 def ConfigRead(name, option):
     config.read("config.ini")
     return config[name][option]
 
-config = configparser.RawConfigParser()
+# Value
 API = ConfigRead("api", "API")
 papagoimage_url = ConfigRead("url", "papagoimage_url")
-langlist = ["ko", "en", "zh-CN", "zh-TW", "es", "fr", "vi", "th", "id"]
+langlist = ["ko", "en", "ja", "zh-CN", "zh-TW", "es", "fr", "vi", "th", "id"]
 headers = {"X-Naver-Client-Id": ConfigRead("option", "id"), "X-Naver-Client-Secret": ConfigRead("option", "secret")}
 translation_url = ConfigRead("url", "translation_url")
-detect_url = ConfigRead("url", "detect_url")
+detect_url = ConfigRead("url", "detect_url")\
 
-class Tool():
+# Color
+BLUE = '\033[34m'
+YELLOW = '\033[33m'
+GREEN = '\033[32m'
+RED = '\033[31m'
+END = '\033[0m'
+BOLD = '\033[1m'
 
-    @classmethod    
+# Tool
+class Tool:
+
+    @classmethod
     def Embed(cls, ctx, title, description):
         embed=discord.Embed(title=f"「{title}」", description=description, color=0x00e00f)
         embed.set_thumbnail(url=papagoimage_url)
         embed.set_footer(text=API)
         return embed
-    
+
     @classmethod
     def TXTRead(cls, name):
         with open(f"txt/{name}.txt", "r", encoding="utf8") as tf:
-            return tf.read()
+            text = tf.read()
+            return text
 
     @classmethod
     def FeedBack(cls, ctx, lang1, lang2, text, result_text, type):
         if ConfigRead("option", "feedback") == "on":
-            print(f"Type: {type}\n")
+            print(f"{GREEN}{BOLD}Type: {type}\n{END}")
             print(f"{ctx.author}: {text} -> {result_text} ({lang1} -> {lang2})\n")
 
-    @classmethod      
+    @classmethod
     def LangChange(cls, lang):
         if lang in langlist:
             result = lang
@@ -43,6 +55,8 @@ class Tool():
             result = "ko"
         elif lang == "영어":
             result = "en"
+        elif lang == "일본어":
+            result = "ja"
         elif lang == "중국어(간체)":
             result = "zh-CN"
         elif lang == "중국어(번체)":
@@ -61,22 +75,25 @@ class Tool():
             result = None
         return result
 
-class Translation():
+# Translation
+class Translation:
 
     @classmethod
     def Translation(cls, lang1, lang2, text):
         translation_text = {"source": lang1, "target": lang2, "text": text}
-        response = rq.post(translation_url, headers=headers, data=translation_text) 
+        response = rq.post(translation_url, headers=headers, data=translation_text)
         result = response.json()
-        return result["message"]["result"]["translatedText"]
-    
+        result_text = result["message"]["result"]["translatedText"]
+        return result_text
+
     @classmethod
     def Detect(cls, text):
         detect_text = {"query": text}
         detect_response = rq.post(detect_url, headers=headers, data=detect_text)
         detect_result = detect_response.json()
-        return detect_result["langCode"]
-    
+        result_text = detect_result["langCode"]
+        return result_text
+
     @classmethod
     def Embed(cls, ctx, lang1, lang2, result_text):
         embed=discord.Embed(title=f"「번역 결과 {lang1} -> {lang2}」", description=f"{ctx.author.display_name}: {result_text}", color=0x00e00f)
