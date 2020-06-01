a7417c118aaf0c6b2e5ca81d0f20d9334b8c507b
Beta5051 <58169688+Beta5051@users.noreply.github.com>
2020-06-01 14:26:37 +0900

    수정

--- Papago Bot/Papago.py
+++ Papago Bot/Papago.py
@@ -1,11 +1,14 @@
+# Moduel
 import subprocess
 from tkinter import *
 
+# UI
 window = Tk()
 window.title("Papago")
 window.iconbitmap(default="Papago.ico")
 window.resizable(False, False)
 
+# Start
 def Start():
     window.destroy()
     subprocess.call(["python", "source/PapagoMain.py"])
@@ -13,6 +16,7 @@ def Start():
 label = Label(text="\n본 프로그램은 디스코드 봇 프로그램으로\nPapago API를 사용하였습니다.")
 label.pack()
 
+# Event
 button = Button(window, text="계속", command=Start)
 button.pack()
 
