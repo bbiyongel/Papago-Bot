baf75db66c60a0da086e43b34421dd3f5ecf4553
Beta5051 <Beta5051@gmail.com>
2020-06-06 01:06:50 +0900

    Update ModuleInstall.py

--- Papago Bot/ModuleInstall.py
+++ Papago Bot/ModuleInstall.py
@@ -3,6 +3,7 @@ import os
 
 # Install
 os.system("python -m pip install --upgrade pip") # Upgrade Pip
+os.system("pip install disocrd.py")
 os.system("pip install requests")
 os.system("pip install gtts")
 os.system("pip install eyed3")
